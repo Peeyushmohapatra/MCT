e152fab766daf350bb59169a2ce93044d3396ae6
Peeyushmohapatra <peeyushm1998@gmail.com>
2023-01-06 22:01:21 +0530

    Done

--- Weather App/script.js
+++ Weather App/script.js
@@ -11,10 +11,16 @@
     const button = document.getElementById("button");
     const city = document.getElementById("city");
     const body = document.getElementsByTagName("body")[0];
+    const container = document.getElementById("container");
+    const left = document.getElementById("left")
 
         // getCoordinates()
-        const backGround = ["Clear_Weather.jpg","Clouds_weather.jpg","Haze_weather.jpg","Rainy_weather.jpg","Smoky_Weather.webp","Sunny_weather.jpg","Snow_weather.jpg"]
-        const weathers = ["Clear.png","Clouds.jpg","Haze.png","Rainy.png","Smokey.png","Sunny.png","Snow.png"]
+        const backGround = ["Clear_Weather.jpg","Clouds_weather.jpg","Haze_weather.jpg","Rainy_Day_image.jpg","Smoky_Weather.webp","Sunny_weather.jpg","Snow_weather.jpg","Mist_weather.jpg"];
+        const weathers = ["Clear.png","Clouds.jpg","Haze.png","Rainy.png","Smokey.png","Sunny.png","Snow.png","Mist.png"];
+
+
+        const nightBackGround = ["Clear_night.jpg","cloudy_night_image.jpg","Haze_night_img.jpg","Rainy_night_image.jpg","Smoky_night_image.jpg","Clear_night.jpg","Snow_night_image.jpg","Mist_night_image.jpg"]
+        const nightIcon = ["Clear_night_icon.jpg","Cloudy_night.png","Haze_night_logo.png","Rainy_night_logo.png","smoky_night_logo.png","Clear_night_icon.jpg","Snow_night_logo.webp","Mist.png"]
 
         init()
         function init(){
@@ -25,7 +31,7 @@
         const getData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=133aca6a3bda2d32fd31c9ab64ad4343`);
         const textData = await getData.text();
         const jsonData = JSON.parse(textData);
-        // console.log(jsonData);
+        console.log(jsonData);
         const lon = jsonData.coord.lon;
         const lat = jsonData.coord.lat;
 
@@ -38,24 +44,54 @@
         const textData = await getData.text();
         const jsonData = JSON.parse(textData);
         console.log(jsonData);
+
+        let date = new Date();
+        let currDate = `${date.getUTCDate()}:${date.getUTCMonth()+1}:${date.getUTCFullYear()}`
+        console.log(currDate);
+
+        let time = date.getUTCHours();
+        time = time+5;
+        console.log(typeof time,time);
+        let currTime = `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`
+        console.log(currTime);
+
+
+
         const weatherIcon = jsonData.weather[0].icon
         city.innerText = jsonData.name;
         humidity.innerText = `${jsonData.main.humidity}%`;
         const km = jsonData.visibility/1000;
-        visibility.innerText = `${km}Km`;
+        visibility.innerText = `${km.toFixed(1)}Km`;
         pressure.innerText = `${jsonData.main.pressure}hPa`;
         const celciouc = jsonData.main.temp - 273.15
         degree.innerHTML = `${celciouc.toFixed(1)}&deg;C`;
         windSpeed.innerText = `${jsonData.wind.speed}kph`
         weather.innerText = jsonData.weather[0].main;
         cloudiness.innerText = `${jsonData.clouds.all}%`
-        console.log(weather);
-        for(let i=0; i<weathers.length; i++){
-            if(weathers[i].includes(weather.innerText)){
-                image.setAttribute("src",weathers[i]);
-                body.style.background = `url(${backGround[i]})`
-                body.style.backgroundSize = "100% 100%"
+        // console.log(weather);
+        
+
+        if(time>=5 && time<=18){
+            
+        }else{
+            container.style.backgroundColor = "rgba(0, 0, 0, 0.598)";
+            left.style.backgroundColor = "rgba(0, 0, 0, 0.550)"
+        }
+        if(time>=5 && time<=18){
+            for(let i=0; i<weathers.length; i++){
+                if(weathers[i].includes(weather.innerText)){
+                    image.setAttribute("src",weathers[i]);
+                    body.style.background = `url(${backGround[i]})`
+                    body.style.backgroundSize = "100% 100%"
+                }
+            }
+        }else{
+            for(let i=0; i<nightIcon.length; i++){
+                if(nightIcon[i].includes(weather.innerText)){
+                    image.setAttribute("src",nightIcon[i]);
+                    body.style.background = `url(${nightBackGround[i]})`
+                    body.style.backgroundSize = "100% 100%"
+                }
             }
         }
-        
     }
