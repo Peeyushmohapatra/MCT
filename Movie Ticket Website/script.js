63c0ee0708d96ea396612e202c6d08a851c9f4d5
Peeyushmohapatra <peeyushm1998@gmail.com>
2023-02-10 12:31:19 +0530

    Done

--- Movie Ticket Website/script.js
+++ Movie Ticket Website/script.js
@@ -1,67 +1,63 @@
-const container = document.querySelector(".seats-container");
-const count = document.getElementById("number-of-seats");
-const total = document.getElementById("number");
-const seats = document.querySelectorAll(".blocks .seat:not(.booked-seat)");
-const selectedMovie = document.getElementById("select");
+const seats = document.getElementsByClassName("seat");
+const price = document.getElementById("number");
 const booking = document.getElementById("number-of-seats");
 const selectedSeats = document.getElementsByClassName("selected-count");
-const ticketPricee = document.getElementById("select");
-let ticketPrice = Number(selectedMovie.value);
+const ticketPrice = document.getElementById("select");
 const priceToshown = document.getElementById("number");
-const containerr = document.getElementsByClassName("seats-container")[0];
-const ticket = document.getElementById("ticket")
-let arrLocalStorage = [...seats];
-getdatafromLocalstorage();
-function setMovieData(movieIdx, moviePrice) {
-  localStorage.setItem("selectedMovieIndex", movieIdx);
-  localStorage.setItem("selectedMoviePrice", moviePrice);
-}
+const container = document.getElementsByClassName("seats-container")[0];
+let arrLocalStorage = [];
+const display = document.getElementById("bookedSeats");
+const ticket = document.getElementById("ticket");
 
-container.addEventListener("click", (e) => {
-    if (
-      e.target.classList.contains("seat") &&
-      !e.target.classList.contains("booked-seat")
-    ) {
-      e.target.classList.toggle("selected");
+
+
+localStorage.setItem("selectedMoviePrice", ticketPrice.value);
+// localStorage.setItem("selectedSeats",arrLocalStorage);
+
+for (let i = 0; i < seats.length; i++) {
+  let flag = true;
+  let ele = seats[i];
+  ele.addEventListener("click", () => {
+    let backColor = "rgb(49,215,169)";
+    let othercolor = "rgb(1,22,62)";
+    // console.log(flag);
+
+    if (flag === true) {
+      flag = false;
+      ele.setAttribute("class", "selected-count");
+      arrLocalStorage.push(i);
+    } else {
+      flag = true;
+      ele.setAttribute("class", "seat");
+      const ans = arrLocalStorage.indexOf(i);
+      arrLocalStorage.splice(ans, 1);
     }
-    display();
+    booking.innerText = selectedSeats.length;
+    priceToshown.innerText = selectedSeats.length * ticketPrice.value;
+    ticket.innerText = JSON.parse(localStorage.getItem("selectedSeats"))
   });
-
-selectedMovie.addEventListener("change", (e) => {
-    ticketPrice = Number(e.target.value);
-    setMovieData(e.target.seatsIdx, e.target.value);
-    display();
-  });
-
-function getdatafromLocalstorage() {
-  const selectedSeats = JSON.parse(localStorage.getItem("selectesSeats"));
-  if (selectedSeats !== null && selectedSeats.length > 0) {
-    seats.forEach((seat, idx) => {
-      if (selectedSeats.indexOf(idx) > -1) {
-        seat.classList.add("selected");
-      }
-    });
-  }
-  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
-  if (selectedMovieIndex !== null) {
-    selectedMovie.seatsIdx = selectedMovieIndex;
-  }
 }
+ticketPrice.addEventListener("change", () => {
+  booking.innerText = selectedSeats.length;
+  priceToshown.innerText = selectedSeats.length * ticketPrice.value;
+  localStorage.setItem("selectedMoviePrice", ticketPrice.value);
+});
+
+setInterval(() => {
+    if(arrLocalStorage !== null && arrLocalStorage.length > 0){
+        localStorage.setItem("selectedSeats", JSON.stringify(arrLocalStorage));
+
+    }
+}, 100);
 
 
-
-
-function display() {
-    const selectedSeats = document.querySelectorAll(".blocks .seat.selected");
-    const seatsIdx = [...selectedSeats].map(function (ele) {
-      return arrLocalStorage.indexOf(ele);
-    });
-    localStorage.setItem("selectesSeats", JSON.stringify(seatsIdx));
-    const selectedSeatsCount = selectedSeats.length;
-  
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
-    ticket.innerText = JSON.parse(localStorage.getItem("selectesSeats"))
-  }
-
-display();
+setTimeout(() => {
+    const ans = JSON.parse(localStorage.getItem("selectedSeats"));
+    console.log(ans);
+    for(let i=0; i<ans.length; i++){
+        let a = ans[i];
+        seats[i].setAttribute("class","selected-count")
+    }
+    
+},1000) 
+// localStorage.clear()
